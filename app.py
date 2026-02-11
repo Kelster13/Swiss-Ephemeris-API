7c3a6ea37e83acfee7c6ddaa67fa190fa1b8660e
Kelster13 <kellyhelton34@gmail.com>
2026-02-11 15:45:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def geocode_location(query: str):
         loc = geolocator.geocode(query, addressdetails=True)
         if loc:
             return loc.latitude, loc.longitude, loc.raw
-    
+     
      except Exception:
         pass  # fall through to fallback
 
